64555a5586b37d62b80868bb200e72ecb23aa7a8
josemsb92 <josemsb92@gmail.com>
2022-03-02 10:51:04 +0100

    getByCriteria now working, but need to implement an "and" on the sentence

--- controllers/pets/getPets.js
+++ controllers/pets/getPets.js
@@ -1,15 +1,19 @@
 const petsManager = require("../../managers/petsManager");
 
 async function getPets(req, res) {
-  if (req.query == '') {
+  if (Object.keys(req.query).length === 0) {
     const pets = await petsManager.getAllPets();
+    if(pets) res.status(200).json(pets)
+    else res.status(404).end();
     
-    res.status(200).json(pets);
    
-  } else {
+  } else {    
     const pets = await petsManager.getByCriteria(req.query);
+    if(pets){
+      res.status(200).json(pets);
+    }else res.status(404).end();
    
-    res.status(200).json(pets);
+    
     
   }
 }
--- managers/petsManager.js
+++ managers/petsManager.js
@@ -24,34 +24,39 @@ class PetsManager {
     }
   }
 
-  // static async getByCriteria(criteria = {}) {
-  //   // varios filtros
-  //   /* ejemplo criteria = {name:pepe, dni:123} */
-  //   console.log(criteria, "Criteria primero");
-  //   const stringifiedObj = Object.entries(criteria).map((x) => x.join("=")).join("AND");
-  //   console.log(stringifiedObj, "Criteria stringified");
-  //   /*Dividimos el objeto en una matriz de pequeñas matrices de parámetros => 
-  //     [ ['name', 'pepe'], ['dni', 123] ]  */
+  static async getByCriteria(criteria = {}) {
+    // varios filtros
+    /* ejemplo criteria = {name:pepe, dni:123} */
+    console.log(criteria, "Criteria primero");
+    // const stringifiedObj = Object.entries(criteria).map((x) => x.join("=")).join("AND");
+    // console.log(stringifiedObj, "Criteria stringified");
+    /*Dividimos el objeto en una matriz de pequeñas matrices de parámetros => 
+      [ ['name', 'pepe'], ['dni', 123] ]  */
 
-  //   /*Convierte la [key, value] salida de la matriz Object.enteries al key=value formato
-  //    [[name = 'pepe'], [dni = 123] ] */
+    /*Convierte la [key, value] salida de la matriz Object.enteries al key=value formato
+     [[name = 'pepe'], [dni = 123] ] */
 
-  //   /* Y esto nos lo dejaría asi no? => ['name'='pepe'] AND ['dni', 123] ] */
+    /* Y esto nos lo dejaría asi no? => ['name'='pepe'] AND ['dni', 123] ] */
 
-  //   sql = ''
-  //   valuesCriteria = []
-  //   criteria.foreach((key, value) => {
-  //      sql += `${key} = $${index}::text AND `
-  //       valuesCriteria.push(value)
-  //   })
+    let sql = ''
+    const valuesCriteria = []
 
-  //   const pets = await adoptionClient.query(
-  //     `SELECT * FROM Pets WHERE ${sql};`,
-  //     ...valuesCriteria
-  //   );
-  //   console.log(pets, 'PETS');
-  //   //Por qué es necesario hacer un new Pet() para un get???
-  //   return pets.rows;
-  // }
+    Object.entries(criteria).forEach(([key, value], index) => {          
+       sql += `${key} = $${index+1}::text `
+        valuesCriteria.push(value)        
+    })   
+   
+    const pets = await adoptionClient.query(
+      `SELECT * FROM Pets WHERE ${sql}`,
+      [...valuesCriteria]
+    );    
+    //Por qué es necesario hacer un new Pet() para un get???
+    const info = pets.rows[0];
+    if (info) {
+      return new Pet(info);
+    } else {
+      return undefined;
+    }
+  }
 }
 module.exports = PetsManager;
