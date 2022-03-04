d3dd94f303dad5735083f3e19bd30a4a26cf3f16
josemsb92 <josemsb92@gmail.com>
2022-03-04 20:21:34 +0100

    pet model attributes changed

--- models/pet.js
+++ models/pet.js
@@ -2,8 +2,8 @@ class Pet {
   id;
   size;
   gender;
-  weightkg;
-  rescuedat;
+  weightKg;
+  rescuedAt;
   birthday;
   species;
   reservationDate; 
@@ -15,8 +15,8 @@ class Pet {
     this.name = props.name;    
     this.size = props.size;
     this.gender = props.gender;
-    this.weightkg = props.weightkg;
-    this.rescuedat = props.rescuedat;
+    this.weightKg = props.weightKg;
+    this.rescuedAt = props.rescuedAt;
     this.birthday = props.birthday;
     this.species = props.species;
     this.reservationDate = props.reservationDate;
