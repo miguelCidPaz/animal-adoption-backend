c6b494ba9ec6ca176ccab2dc3342a48e8f62aca4
fernandocieri <fernandocieri@gmail.com>
2022-03-06 18:58:03 +0100

    Adds more formatting to the data brought from the database

--- managers/petsManager.js
+++ managers/petsManager.js
@@ -1,15 +1,40 @@
 const adoptionClient = require("./conection");
 const Pet = require("../models/pet");
 
-function createImagesArray(info) {
-  const newInfo = info.map((pet) => {
-    if (pet.images) {
+const databaseKeys = {
+  dog: 1,
+  cat: 2,
+  male: 1,
+  female: 2,
+  small: 1,
+  medium: 2,
+  large: 3
+}
+
+function format(info) {
+  let newInfo = [];
+  info.map((pet) => {
+    let newPet = { ...pet };
+    if (newPet.images) {
       const urlArray = pet.images.split(',');
-      const newPet = { ...pet, images: urlArray };
-      return newPet
-    } else {
-      return pet
+      newPet = { ...newPet, images: urlArray };
     }
+    if (newPet.species) {
+      (newPet.species === databaseKeys.dog) ? newPet = { ...newPet, species: 'Dog' } : newPet = { ...newPet, species: 'Cat' }
+    }
+    if (newPet.gender) {
+      (newPet.gender === databaseKeys.male) ? newPet = { ...newPet, gender: 'Male' } : newPet = { ...newPet, gender: 'Female' }
+    }
+    if (newPet.size) {
+      if (newPet.size === databaseKeys.small) {
+        newPet = { ...newPet, size: 'Small' }
+      } else if (newPet.size === databaseKeys.medium) {
+        newPet = { ...newPet, size: 'Medium' }
+      } else {
+        newPet = { ...newPet, size: 'Large' }
+      }
+    }
+    newInfo.push(newPet);
   })
   return newInfo
 }
@@ -17,7 +42,7 @@ function createImagesArray(info) {
 class PetsManager {
   static async getAllPets() {
     const pets = await adoptionClient.query(`SELECT * FROM Pets;`);
-    const formattedInfo = createImagesArray(pets.rows);
+    const formattedInfo = format(pets.rows);
     return formattedInfo.map((pet) => {
       return new Pet(pet);
     });
@@ -27,7 +52,7 @@ class PetsManager {
     const pet = await adoptionClient.query(
       `SELECT * FROM Pets WHERE id = '${id}';`
     );
-    const formattedInfo = createImagesArray(pet.rows);
+    const formattedInfo = format(pet.rows);
     if (formattedInfo) {
       return new Pet(formattedInfo[0]);
     } else {
@@ -54,7 +79,7 @@ class PetsManager {
     });
 
     const pets = await adoptionClient.query(`SELECT * FROM Pets WHERE ${sql}`);
-    const formattedInfo = createImagesArray(pets.rows);
+    const formattedInfo = format(pets.rows);
     if (formattedInfo) {
       return formattedInfo.map((petData) => {
         return new Pet(petData);
