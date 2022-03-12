dbe0619167d79f034961228d537310e9fd3bfb48
josemsb92 <josemsb92@gmail.com>
2022-03-12 13:19:03 +0100

    cors added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express()
 port = 3000;
-
+app.use(cors())
 app.use(require('express').json());
 app.use('/api', require('./routes'));
 
